3f7224152d65d32f33d0f592c6cb1fb1ef9ce415
minnukota381 <minnukota381@gmail.com>
2024-06-30 19:34:53 +0530

    feat: "Removed default App component and replaced with React Router setup for Home and VideoDetail pages"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import VideoDetail from './pages/VideoDetail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/video/:id" component={VideoDetail} />
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
