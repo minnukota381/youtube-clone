c624b989b0410b46c7bafa0042fafa7071758c92
minnukota381 <minnukota381@gmail.com>
2024-06-30 21:19:13 +0530

    fix: Integrate environment variables for YouTube API key security

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -1,8 +1,84 @@
-// src/services/videoService.ts
 import axios from 'axios';
 import { Video } from '../types/video';
 
-export const getVideos = async (): Promise<Video[]> => {
-    const response = await axios.get('/mockData/videos.json'); // Adjust the path to your mock data
-    return response.data;
+const API_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+if (!API_KEY) {
+    throw new Error('REACT_APP_YOUTUBE_API_KEY is not defined in the environment variables.');
+}
+
+const headers = {
+    'x-rapidapi-host': 'youtube-v31.p.rapidapi.com',
+    'x-rapidapi-key': API_KEY,
+};
+
+interface VideoItem {
+    id: string;
+    snippet: {
+        title: string;
+        description: string;
+        thumbnails: {
+            default: {
+                url: string;
+            };
+        };
+    };
+}
+
+export const searchVideos = async (relatedToVideoId: string, maxResults: number): Promise<Video[]> => {
+    try {
+        const response = await axios.get(`${API_URL}/search`, {
+            headers,
+            params: {
+                relatedToVideoId,
+                part: 'id,snippet',
+                type: 'video',
+                maxResults,
+            },
+        });
+
+        const videos: Video[] = response.data.items.map((item: VideoItem) => ({
+            id: item.id,
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnail: item.snippet.thumbnails.default.url,
+            url: `https://www.youtube.com/watch?v=${item.id}`,
+            comments: [],
+        }));
+
+        return videos;
+    } catch (error) {
+        console.error('Error fetching videos:', error);
+        return [];
+    }
+};
+
+export const fetchRelatedVideos = async (relatedToVideoId: string, maxResults: number): Promise<Video[]> => {
+    try {
+        const response = await axios.get(`${API_URL}/search`, {
+            headers,
+            params: {
+                relatedToVideoId,
+                part: 'id,snippet',
+                type: 'video',
+                maxResults,
+            },
+        });
+
+        const videos: Video[] = response.data.items.map((item: VideoItem) => ({
+            id: item.id,
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnail: item.snippet.thumbnails.default.url,
+            url: `https://www.youtube.com/watch?v=${item.id}`,
+            comments: [],
+        }));
+
+        return videos;
+    } catch (error) {
+        console.error('Error fetching related videos:', error);
+        return [];
+    }
 };
